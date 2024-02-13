d253d6552d7b85d02692c5a7b126d5e13979f6ef
amad-nicolas <103444403+amad-nicolas@users.noreply.github.com>
2024-02-13 10:19:44 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,34 +1,61 @@
-class ListaDeCompras:
-    def __init__(self):
-        self.lista = []
-    
-    def inserirItem(self):
-        while True:
-            item = input('Digite o nome do item: ')
-            self.lista.append(item)
-            continuar = input('Digite 1 para continuar ou 2 para sair: ')
-            
-            if continuar == '1':
-                pass
-            elif continuar == '2':
-                break 
-            else:
-                print('Você digitou um valor inválido')
-    
-    def exibir(self):
-        i=0
-        while i<len(self.lista):
-            print(f'{i+1}.{self.lista[i]}')
-            i+=1
-    
-    def inserirValor(self):
+import os
+import time
+import pandas
+
+class Item:
+    def __init__(self,nome,preco=0,quantidade=0,) :
+        self.nome=nome
+        self.preco=preco
+        self.quantidade=quantidade
+    def subTotal(self):
+        return self.preco*self.quantidade
+
+
+def fechar():
+    global run,df
+    encerrar=int(input('Digite qualquer tecla para continuar ,caso queira encerrar digite 1: '))
+    if encerrar==1:
         total=0
-        while True:
-            valor=float(input('digite o preço do item: '))
-            quantidade=int(input('digite quantos itens ira comprar: '))
-            total+=valor*quantidade
+        t=0
+        while t<5:
+            total+=(df.iloc[t,3]).astype(float)
+            t+=1
+        
+        print(f'o total foi de {total}')
+        time.sleep(5)
+        run=False
+        
+    
+listaDeCompras=[]
+c=0
+while c<5:
+    item=[]
+    nome=input('digite o nome do item:')
+    i=Item(nome,0,0)
+    item=[i.nome,i.preco,i.quantidade,0]
+    listaDeCompras.append(item)
+    c+=1
+os.system('cls')
+df=pandas.DataFrame(listaDeCompras)
+print(df)
+
+        
+run=True
+
+while run:
+    indice=int(input('Digite o indice do item que deseja atualizar: '))
+    preco=float(input('Digite o preço unitário do item: '))
+    quantidade=int(input('digite a quantidade que ira comprar: '))
+    df.iloc[indice,1]=preco
+    df.iloc[indice,2]=quantidade
+    df.iloc[indice,3]=preco*quantidade
+    os.system('cls')
+    
+    fechar()
+    os.system('cls')
+    print(df)
 
 
-minhaLista=ListaDeCompras()
-minhaLista.inserir()
+   
 
+        
